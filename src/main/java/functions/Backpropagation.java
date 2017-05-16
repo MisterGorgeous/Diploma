9399186d05254c1c1cbf195cf6184bc9886e9341
MisterGorgeous <slabadniak2401@gmail.com>
2017-05-16 17:25:29 +0300

    fming integration started

--- src/main/java/functions/Backpropagation.java
+++ src/main/java/functions/Backpropagation.java
@@ -2,23 +2,46 @@ package functions;
 
 
 import data.HandWritenDigits;
+import de.jungblut.math.DoubleVector;
 import de.jungblut.math.dense.DenseDoubleMatrix;
 import de.jungblut.math.dense.DenseDoubleVector;
+import de.jungblut.math.minimize.CostFunction;
+import de.jungblut.math.minimize.CostGradientTuple;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Backpropagation {
-    private static DenseDoubleMatrix theta1 = RandomInitialization.randInitialize(256,16,0.12);
-    private static DenseDoubleMatrix theta2 = RandomInitialization.randInitialize(16,10,0.12);
-    private static DenseDoubleMatrix theta1_grad = new DenseDoubleMatrix(257,16,0);
-    private static DenseDoubleMatrix theta2_grad = new DenseDoubleMatrix(17,10,0);
-    //private static double J;
+public class Backpropagation implements CostFunction {
+    private static DenseDoubleMatrix theta1 = RandomInitialization.randInitialize(256, 16, 0.12);
+    private static DenseDoubleMatrix theta2 = RandomInitialization.randInitialize(16, 10, 0.12);
+    private static DenseDoubleMatrix theta1_grad = new DenseDoubleMatrix(257, 16, 0);
+    private static DenseDoubleMatrix theta2_grad = new DenseDoubleMatrix(17, 10, 0);
     private static final double lambda = 1;
 
 
-    public static void calculate(){
-        for(int i=0; i< HandWritenDigits.X_SIZE; ++i){
+    public static DenseDoubleMatrix getTheta1() {
+        DenseDoubleMatrix copy = new DenseDoubleMatrix(theta1.toArray());
+        return copy;
+    }
+
+    public static DenseDoubleMatrix getTheta2() {
+        DenseDoubleMatrix copy = new DenseDoubleMatrix(theta2.toArray());
+        return copy;
+    }
+
+    public static DenseDoubleMatrix getTheta1_grad() {
+        DenseDoubleMatrix copy = new DenseDoubleMatrix(theta1_grad.toArray());
+        return copy;
+    }
+
+    public static DenseDoubleMatrix getTheta2_grad() {
+        DenseDoubleMatrix copy = new DenseDoubleMatrix(theta2_grad.toArray());
+        return copy;
+    }
+
+
+    public static void calculate() {
+        for (int i = 0; i < HandWritenDigits.X_SIZE; ++i) {
             DenseDoubleMatrix a1 = Normalization.addA0asColumn(HandWritenDigits.getXRowMat(i));
             DenseDoubleMatrix z2 = (DenseDoubleMatrix) a1.multiply(theta1.transpose());
             DenseDoubleMatrix sigZ2 = Sigmoid.sigmoid(z2);
@@ -38,16 +61,16 @@ public class Backpropagation {
 
             DenseDoubleMatrix theta2_gradWithoutNorm = Normalization.dellA0AsColumn(theta2_grad.transpose()).transpose();
             theta2_gradWithoutNorm = (DenseDoubleMatrix) theta2_gradWithoutNorm.multiply(lambda).add(theta2_gradWithoutNorm);
-            theta2_grad = (DenseDoubleMatrix) Normalization.addColumn(theta2_gradWithoutNorm.transpose(),theta2_grad.getColumn(0)).divide(HandWritenDigits.X_SIZE).transpose();
+            theta2_grad = (DenseDoubleMatrix) Normalization.addColumn(theta2_gradWithoutNorm.transpose(), theta2_grad.getColumn(0)).divide(HandWritenDigits.X_SIZE).transpose();
 
             DenseDoubleMatrix theta1_gradWithoutNorm = Normalization.dellA0AsColumn(theta1_grad.transpose()).transpose();
             theta1_gradWithoutNorm = (DenseDoubleMatrix) theta1_gradWithoutNorm.multiply(lambda).add(theta1_gradWithoutNorm);
-            theta1_grad = (DenseDoubleMatrix) Normalization.addColumn(theta1_gradWithoutNorm.transpose(),theta1_grad.getColumn(0)).divide(HandWritenDigits.X_SIZE).transpose();
+            theta1_grad = (DenseDoubleMatrix) Normalization.addColumn(theta1_gradWithoutNorm.transpose(), theta1_grad.getColumn(0)).divide(HandWritenDigits.X_SIZE).transpose();
 
         }
     }
 
-    public static double calculateCost(DenseDoubleMatrix theta1,DenseDoubleMatrix theta2){
+    public static double calculateCost(DenseDoubleMatrix theta1, DenseDoubleMatrix theta2) {
         double J;
         DenseDoubleMatrix x = HandWritenDigits.getX();
         x = Normalization.addA0asColumn(x);
@@ -60,7 +83,7 @@ public class Backpropagation {
         DenseDoubleMatrix y = HandWritenDigits.getYBinaryFormat();
         DenseDoubleMatrix check = (DenseDoubleMatrix) y.multiplyElementWise(MatrixOperations.log(h));
         DenseDoubleMatrix check1 = (DenseDoubleMatrix) y.subtractBy(1).multiplyElementWise(MatrixOperations.log(h.subtractBy(1)));
-        J = check.multiply(-1).subtract(check1).sum()/HandWritenDigits.X_SIZE;
+        J = check.multiply(-1).subtract(check1).sum() / HandWritenDigits.X_SIZE;
         DenseDoubleMatrix theta1FromSec = Normalization.dellA0AsColumn(theta1);
         DenseDoubleMatrix theta2FromSec = Normalization.dellA0AsColumn(theta2);
         double reg = theta1FromSec.multiplyElementWise(theta1FromSec).sum() + theta2FromSec.multiplyElementWise(theta2FromSec).sum();
@@ -68,24 +91,14 @@ public class Backpropagation {
     }
 
 
+    @Override
+    public CostGradientTuple evaluateCost(DoubleVector doubleVector) {
+        theta1 = new DenseDoubleMatrix(257,16,doubleVector.slice(257*16).toArray());
+        theta2 = new DenseDoubleMatrix(17,10,doubleVector.slice(257*16,doubleVector.getLength()).toArray());
 
-    public static DenseDoubleMatrix getTheta1() {
-        return theta1;
+        calculate();
+
+
+        return  new CostGradientTuple();
     }
-
-    public static DenseDoubleMatrix getTheta2() {
-        return theta2;
-    }
-
-    public static DenseDoubleMatrix getTheta1_grad() {
-        return theta1_grad;
-    }
-
-    public static DenseDoubleMatrix getTheta2_grad() {
-        return theta2_grad;
-    }
-
-    /*public static double getJ() {
-        return J;
-    }*/
 }
--- src/main/java/functions/NumericalGradient.java
+++ src/main/java/functions/NumericalGradient.java
@@ -6,9 +6,12 @@ import de.jungblut.math.dense.DenseDoubleMatrix;
 public class NumericalGradient {
     private static DenseDoubleMatrix numGradientTheta1 = RandomInitialization.randInitialize(256,16,0);
     private static DenseDoubleMatrix numGradientTheta2 = RandomInitialization.randInitialize(16,10,0);
-    private static DenseDoubleMatrix epsilonMatrix1 = new DenseDoubleMatrix(16,257,0);
-    private static DenseDoubleMatrix epsilonMatrix2 = new DenseDoubleMatrix(10,17,0);
     private static double epsilon = 0.0001;
+    private static double minusEpsilon;
+    private static double plussEpsilon;
+    private static double grad;
+    private static DenseDoubleMatrix theta1 = Backpropagation.getTheta1();
+    private static DenseDoubleMatrix theta2 = Backpropagation.getTheta2();
 
     public static DenseDoubleMatrix getNumGradientTheta1() {
         return numGradientTheta1;
@@ -19,33 +22,23 @@ public class NumericalGradient {
     }
 
     public static void calculateNumGrad(){
-        double minusEpsilon;
-        double plussEpsilon;
-        double grad;
+        calculateTheta(theta1,numGradientTheta1);
+        calculateTheta(theta2,numGradientTheta2);
+    }
 
-        for (int i = 0; i < numGradientTheta1.getRowCount(); ++i) {
-            for (int j = 0; j < numGradientTheta1.getColumnCount(); ++j) {
+    private static void calculateTheta( DenseDoubleMatrix theta,DenseDoubleMatrix numGrad){
+
+        for (int i = 0; i < numGrad.getRowCount(); ++i) {
+            for (int j = 0; j < numGrad.getColumnCount(); ++j) {
                 //result.set(i, j, Math.log(matrix.get(i, j)));
-                epsilonMatrix1.set(i,j,epsilon);
-                minusEpsilon = Backpropagation.calculateCost((DenseDoubleMatrix) Backpropagation.getTheta1().subtract(epsilonMatrix1),Backpropagation.getTheta2());
-                plussEpsilon = Backpropagation.calculateCost((DenseDoubleMatrix) Backpropagation.getTheta1().add(epsilonMatrix1),Backpropagation.getTheta2());
+                theta.set(i,j,theta.get(i,j) - epsilon);
+                minusEpsilon = Backpropagation.calculateCost(theta1,theta2);
+                theta.set(i,j,theta.get(i,j) + 2*epsilon);
+                plussEpsilon = Backpropagation.calculateCost(theta1,theta2);
                 grad = (plussEpsilon - minusEpsilon)/(2*epsilon);
-                numGradientTheta1.set(i,j,grad);
-                epsilonMatrix1.set(i,j,0);
+                numGrad.set(i,j,grad);
+                theta.set(i,j,theta.get(i,j) - epsilon);
             }
         }
-
-        for (int i = 0; i < numGradientTheta2.getRowCount(); ++i) {
-            for (int j = 0; j < numGradientTheta2.getColumnCount(); ++j) {
-                //result.set(i, j, Math.log(matrix.get(i, j)));
-                epsilonMatrix2.set(i,j,epsilon);
-                minusEpsilon = Backpropagation.calculateCost( Backpropagation.getTheta1(), (DenseDoubleMatrix) Backpropagation.getTheta2().subtract(epsilonMatrix2));
-                plussEpsilon = Backpropagation.calculateCost( Backpropagation.getTheta1(), (DenseDoubleMatrix) Backpropagation.getTheta2().add(epsilonMatrix2));
-                grad = (plussEpsilon - minusEpsilon)/(2*epsilon);
-                numGradientTheta2.set(i,j,grad);
-                epsilonMatrix2.set(i,j,0);
-            }
-        }
-
     }
 }
